a3e76f9b672a3eccc9cb74ae9a14c2bf2fbf8893
fravila08 <fr4v1l4@gmail.com>
2024-01-18 23:05:49 -0700

    refactor and comment code

--- backend/daily_workout_app/serializers.py
+++ backend/daily_workout_app/serializers.py
@@ -4,8 +4,8 @@ from .models import Day, Workout
 from user_app.serializers import UserSerializer
 import pytz
 
-
 class OnlyDaySerializer(ModelSerializer):
+    # SerializerMethodField to customize the 'date' field
     date = SerializerMethodField()
 
     class Meta:
@@ -13,14 +13,15 @@ class OnlyDaySerializer(ModelSerializer):
         fields = ["date"]
 
     def get_date(self, obj):
+        # Convert the date to the specified timezone
         pst_timezone = pytz.timezone("America/Los_Angeles")
         utc_timezone = timezone.make_aware(
             timezone.datetime(obj.date.year, obj.date.month, obj.date.day)
         )
         return utc_timezone.astimezone(pst_timezone).date()
 
-
 class WorkoutSerializer(ModelSerializer):
+    # Include OnlyDaySerializer and UserSerializer for related fields
     day = OnlyDaySerializer(read_only=True)
     user = UserSerializer(read_only=True)
 
@@ -28,10 +29,10 @@ class WorkoutSerializer(ModelSerializer):
         model = Workout
         fields = ["id", "day", "user", "name"]
 
-
 class DaySerializer(ModelSerializer):
+    # Include WorkoutSerializer for the 'workouts' field
     workouts = WorkoutSerializer(many=True)
 
     class Meta:
         model = Day
-        fields = ["id","date", "workouts"]
+        fields = ["id", "date", "workouts"]
--- backend/daily_workout_app/views.py
+++ backend/daily_workout_app/views.py
@@ -4,52 +4,50 @@ from user_app.views import UserPermissions
 from rest_framework.response import Response
 from .serializers import DaySerializer, WorkoutSerializer, Day, Workout
 from rest_framework.status import (
-    HTTP_200_OK,
-    HTTP_204_NO_CONTENT,
-    HTTP_401_UNAUTHORIZED,
-    HTTP_500_INTERNAL_SERVER_ERROR,
     HTTP_201_CREATED,
     HTTP_400_BAD_REQUEST,
 )
 
-# Create your views here.
-
-
 class All_Days(UserPermissions):
     def post(self, request):
+        # Assign the current user to the request data
         request.data["user"] = request.user
         new_day = DaySerializer(data=request.data)
         if new_day.is_valid():
             new_day.save()
-            return Response(
-                "Congrats new daily activity was created!", HTTP_201_CREATED
-            )
+            return Response("Congrats new daily activity was created!", HTTP_201_CREATED)
         else:
             print(new_day.error_messages)
             return Response(new_day.error_messages, status=HTTP_400_BAD_REQUEST)
 
     def get(self, request):
+        # Retrieve and return all days for the current user
         return Response(DaySerializer(request.user.days.all(), many=True).data)
 
 
 class A_day(UserPermissions):
-    def get_a_day(self, request, id):
-        return get_object_or_404(request.user.days, id=id)
+    def get_a_day(self, request, date):
+        # Get a specific day for the current user
+        return get_object_or_404(request.user.days, date=date)
 
-    def get(self, request, id):
-        return Response(DaySerializer(self.get_a_day(request, id)).data)
+    def get(self, request, date):
+        # Retrieve and return details of a specific day
+        return Response(DaySerializer(self.get_a_day(request, date)).data)
 
-    def delete(self, request, id):
-        day = self.get_a_day(request, id)
+    def delete(self, request, date):
+        # Delete a specific day and its associated workouts
+        day = self.get_a_day(request, date)
         day.delete()
         return Response("Day and workouts on this day have been deleted")
 
 
 class All_workouts(UserPermissions):
     def get(self, request):
+        # Retrieve and return all workouts for the current user
         return Response(WorkoutSerializer(request.user.workouts.all(), many=True).data)
 
     def post(self, request):
+        # Create a new workout for the current user
         user = request.user
         day, created = Day.objects.get_or_create(
             date=request.data.get("date", timezone.now().date()), user=user
@@ -64,18 +62,20 @@ class All_workouts(UserPermissions):
             return Response(new_workout_ser.data, status=HTTP_201_CREATED)
         else:
             print("failed to create workout")
-            # print(new_workout_ser.error_messages)
             return Response(new_workout_ser.error_messages, status=HTTP_400_BAD_REQUEST)
 
 
 class A_workout(UserPermissions):
     def get_a_workout(self, request, id):
+        # Get a specific workout for the current user
         return get_object_or_404(request.user.workouts, id=id)
 
     def get(self, request, id):
+        # Retrieve and return details of a specific workout
         return Response(WorkoutSerializer(self.get_a_workout(request, id)).data)
 
     def put(self, request, id):
+        # Update details of a specific workout
         workout = self.get_a_workout(request, id)
         updated_workout = WorkoutSerializer(workout, data=request.data, partial=True)
         if updated_workout.is_valid():
@@ -86,6 +86,7 @@ class A_workout(UserPermissions):
             return Response(updated_workout.error_messages, status=HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
+        # Delete a specific workout and its associated day if necessary
         workout = self.get_a_workout(request, id)
         day = workout.day
         workout.delete()
